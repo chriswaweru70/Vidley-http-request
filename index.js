ccf4ee6e30e61a08579cfa8513af1ed6d5704c6e
Chris Waweru <chriswaweru70@gmail.com>
2019-05-31 12:06:51 +0300

    Vidly http requests

---
+++ index.js
@@ -0,0 +1,71 @@
+const Joi = require('joi');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const genres = [
+    { id: 1, name: 'Action'},
+    { id: 2, name: 'Romance'},
+    { id: 3, name: 'Science-fiction'},
+    { id: 4, name: 'Horror'}
+];
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Index of Movies');
+});
+
+app.get('/api/genres', (req, res) => {
+    res.send(genres);
+});
+
+app.get('/api/genres/:id', (req, res) => {
+  const genre = genres.find(g => g.id === parseInt(req.params.id));
+  if(!genre) return res.status(404).send('We have no genre with that id!!!!!');
+  res.send(genre);
+});
+
+app.post('/api/genres', (req, res) => {
+   const { error } = validateGenre(req.body);
+   if (error) return res.status(400).send(error.details[0].message);
+
+   const genre = {
+       id: genres.length + 1,
+       name: req.body.name
+   };
+   genres.push(genre)
+   res.send(genre)
+});
+
+app.put('/api/genres/:id', (req, res) => {
+     const genre = genres.find(g => g.id === parseInt(req.params.id));
+     if(!genre) return res.status(404).send('The Genre with the given ID was not found!');
+
+     const { error } = validateGenre(req.body);
+     if (error) return res.status(400).send(error.details[0].message);
+
+     genre.name = req.body.name;
+
+     res.send(genre)
+});
+
+app.delete('/api/genres/:id', (req, res) => {
+   const genre = genres.find(g => g.id === parseInt(req.params.id));
+   if (!genre) return res.status(404).send('We cannot Delete a Genre with this give ID')
+
+   const index = genres.indexOf(genre);
+   genres.splice(index, 1);
+
+   res.send(genre);
+});
+
+function validateGenre(genre) {
+  const schema = {
+    name:Joi.string().min(4).required()
+  };
+  return Joi.validate(genre, schema);
+}
+
+const port =process.env.PORT || 3000;
+app.listen(port, () => 
+    console.log(`Listening on Port ${port}...`));
