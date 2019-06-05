a1bd3a4079532352de542e21b5a3a478089ad1a3
Chris Waweru <chriswaweru70@gmail.com>
2019-06-05 11:52:13 +0300

    Embedding documents & using populate

---
+++ UsingArrays.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
+const authorSchema = new mongoose.Schema({
+  name: String,
+  bio: String,
+  website: String
+});
+
+const Author = mongoose.model('Author', authorSchema);
+
+const Course = mongoose.model('Course', new mongoose.Schema({
+  name: String,
+  authors: [authorSchema]
+}));
+
+async function createCourse(name, authors) {
+  const course = new Course({
+    name, 
+    authors
+  }); 
+  
+  const result = await course.save();
+  console.log(result);
+}
+
+async function listCourses() { 
+  const courses = await Course.find();
+  console.log(courses);
+}
+
+async function addAuthor(courseId, author) {
+  const course = await Course.findById(courseId);
+  course.authors.push(author);
+  course.save();
+}
+
+async function removeAuthor(courseId, authorId) {
+  const course = await Course.findById(courseId);
+  const author = course.authors.id(authorId);
+  author.remove();
+  course.save();
+  
+}
+
+//addAuthor('5cf77b76052dc11d00499632', new Author({ name: 'Amy'}))
+
+removeAuthor('5cf77b76052dc11d00499632', '5cf7809e138c322bfc75e891')
+
+// createCourse('Node Course', [
+// new Author({ name: 'Mosh' }),
+// new Author({ name: 'Chris' })
+// ]);
